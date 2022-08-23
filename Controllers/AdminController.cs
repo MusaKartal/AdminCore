6d47c25186ccb0bddcabdfd2453f248c326dfba8
MusaKartal <musakartal71@gmail.com>
2022-08-23 21:34:02 +0300

    FirstCommit

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -11,13 +11,7 @@ namespace AdminCore.Controllers
     [AdminFilter]
     public class AdminController : Controller
     {
-        //todo
-        // bussines katmanı oluşturup tüm repositor işlerini orada hallet ***
-        // servis katmanında foto ekleme alanı yarat 
-        // servis katmanında özel şifreleme alanı yarat ve admin kayıt işlemleri yap (sessions ile )**
-        // admin controllerine kayıtlı admin dışında başka kimse giremesin**
-        //front kısımları hallet
-        //bussiness layerini geliştir  
+        
       
         private readonly IProduct _services;
 
