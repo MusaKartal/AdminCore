c5c23d991315a9911d4d2a4cff53a33c55c2d7fe
MusaKartal <musakartal71@gmail.com>
2022-08-30 00:20:21 +0300

    08/30 Front_Update

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -11,7 +11,14 @@ namespace AdminCore.Controllers
     [AdminFilter]
     public class AdminController : Controller
     {
-        
+        //todo
+        // bussines katmanı oluşturup tüm repositor işlerini orada hallet ***
+        // servis katmanında foto ekleme alanı yarat 
+        // servis katmanında özel şifreleme alanı yarat ve admin kayıt işlemleri yap (sessions ile )**
+        // admin controllerine kayıtlı admin dışında başka kimse giremesin**
+        // front kısımları hallet
+        //bussiness layerini geliştir  
+        // session süre tanımla 
       
         private readonly IProduct _services;
 
